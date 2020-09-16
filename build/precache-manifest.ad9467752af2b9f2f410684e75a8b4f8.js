7480a304bff86b2c417fd68716f462d3a2448e2e
Labaks <66236069+Ejay02@users.noreply.github.com>
2020-09-16 05:36:46 +0100

    Add files via upload

---
+++ build/precache-manifest.ad9467752af2b9f2f410684e75a8b4f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74823109374321872872c518a81faca6",
+    "url": "/monsters-r/index.html"
+  },
+  {
+    "revision": "cf8c18436913881c9585",
+    "url": "/monsters-r/static/css/main.fb8aea61.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-r/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-r/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8c18436913881c9585",
+    "url": "/monsters-r/static/js/main.301d79c7.chunk.js"
+  },
+  {
+    "revision": "a6ce5c9a0838578f9192",
+    "url": "/monsters-r/static/js/runtime-main.39b1fe86.js"
+  }
+]);
